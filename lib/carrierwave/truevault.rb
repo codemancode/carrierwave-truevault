7fb99b0b9d8e1eb4c1cc0d978b45ecdfe6f0f9ce
Matthew McFarling <matthew.mcfarling@gmail.com>
2014-09-15 21:20:52 -0400

    adding config blocks

--- lib/carrierwave/truevault.rb
+++ lib/carrierwave/truevault.rb
@@ -1,7 +1,12 @@
+require 'carrierwave'
+require 'carrierwave/storage/truevault'
 require "carrierwave/truevault/version"
 
-module Carrierwave
-  module Truevault
-    # Your code goes here...
+class CarrierWave::Uploader::Base
+  add_config :truevault_api_key
+  add_config :truevault_vault_id
+
+  configure do |config|
+    config.storage_engines[:truevault] = 'CarrierWave::Storage::TrueVault'
   end
 end
